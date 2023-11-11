32e3ed3a087c0962cd8ef0bbc76aa14ae8132c7f
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-11-11 14:58:00 -0500

    Initial Commit

---
+++ CoffeeculeTDD/CoffeeculeTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeculeTDDApp.swift
+//  CoffeeculeTDD
+//
+//  Created by Cory Tripathy on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeculeTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
