b1813ef8e4216844ce7aef4e8f2777c9b7642104
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-11-15 14:20:39 -0500

    test usermanger fails if no account. test init assigns user on success

--- CoffeeculeTDD/Models/User.swift
+++ CoffeeculeTDD/Models/User.swift
@@ -7,3 +7,32 @@
 
 import CloudKit
 
+struct User: Record {
+    static let recordType = "CoffeeculeUser"
+    
+    enum RecordKeys: String, CaseIterable {
+        case systemUserID, name
+    }
+    
+    var id: String
+    var name: String
+    var systemUserID: String
+    var creationDate: Date?
+    
+    init?(from record: CKRecord) {
+        guard let name = record["name"] as? String,
+        let systemUserID = record["systemUserID"] as? String else {
+            return nil
+        }
+        self.creationDate = record.creationDate
+        self.id = record.recordID.recordName
+        self.name = name
+        self.systemUserID = systemUserID
+    }
+    
+    init(systemUserID: String) {
+        self.name = "TEST"
+        self.id = UUID().uuidString
+        self.systemUserID = systemUserID
+    }
+}
--- CoffeeculeTDD/UserManager.swift
+++ CoffeeculeTDD/UserManager.swift
@@ -6,47 +6,24 @@
 //
 
 import SwiftUI
-import CloudKit
-
-struct User: Record {
-    static let recordType = "CoffeeculeUser"
-    
-    enum RecordKeys: String, CaseIterable {
-        case systemUserID, name
-    }
-    
-    var id: String
-    var name: String
-    var systemUserID: String
-    var creationDate: Date?
-    
-    init?(from record: CKRecord) {
-        guard let name = record["name"] as? String,
-        let systemUserID = record["systemUserID"] as? String else {
-            return nil
-        }
-        self.creationDate = record.creationDate
-        self.id = record.recordID.recordName
-        self.name = name
-        self.systemUserID = systemUserID
-    }
-    
-    init(name: String) {
-        self.name = name
-        self.id = UUID().uuidString
-        systemUserID = ""
-    }
-}
 
 class UserManager<Container: DataContainer> {
     private var cloudKitService: CloudKitService<Container>?
     var user: User?
+    var coffeecules: [Coffeecule]?
     @Published var isLoading: Bool
+    @Published var displayedError: Error?
     
     init(with container: Container) {
         isLoading = true
         Task {
-            self.cloudKitService = try await CloudKitService(with: container)
+            do {
+                self.cloudKitService = try await CloudKitService(with: container)
+                self.user = await cloudKitService?.user
+            } catch {
+                displayedError = error
+            }
+            isLoading = false
         }
     }
 }
