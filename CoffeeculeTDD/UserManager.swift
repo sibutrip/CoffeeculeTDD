1ba734ffc30b9ae4ef54fea5c7c268722093f135
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-11-16 15:31:23 -0500

    create coffeecule fails test

--- CoffeeculeTDD/UserManager.swift
+++ CoffeeculeTDD/UserManager.swift
@@ -18,27 +18,28 @@ class UserManager<CKService: CKServiceProtocol> {
     @Published var isLoading: Bool = true
     @Published var displayedError: Error?
     
-    
     enum UserManagerError: Error {
-        case failedToFindUser
+        case noCKServiceAvailable
     }
     
     func createCoffeecule() async throws {
-        if let user = await user,
-           let ckService {
-            let coffeecule = Coffeecule()
-            let relationship = Relationship(with: user, in: coffeecule)
-            try await ckService.save(relationship, withParent: user)
-            self.coffeecules.append(coffeecule)
+        guard let user = await user,
+              let ckService else {
+            throw UserManagerError.noCKServiceAvailable
         }
+        let coffeecule = Coffeecule()
+        let relationship = Relationship(with: user, in: coffeecule)
+        try await ckService.save(relationship, withParent: user)
+        self.coffeecules.append(coffeecule)
     }
     
     func fetchCoffeecules() async throws {
-        if let user = await user,
-           let ckService {
-            let relationships: [Relationship] = try await ckService.children(of: user)
-            self.coffeecules = relationships.compactMap { $0.secondParent }
+        guard let user = await user,
+              let ckService else {
+            throw UserManagerError.noCKServiceAvailable
         }
+        let relationships: [Relationship] = try await ckService.children(of: user)
+        self.coffeecules = relationships.compactMap { $0.secondParent }
     }
     
     init() { }
