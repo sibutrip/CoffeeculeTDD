e6f8132e1caec289d6e223caa4df94a384017ec4
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-11-27 16:48:13 -0500

    im a little combine boy now ;)

--- CoffeeculeTDD/Views/AllMembersView.swift
+++ CoffeeculeTDD/Views/AllMembersView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import CloudKit
 import Charts
+import Combine
 
 struct AllMembersView: View {
     @State var share: CKShare?
@@ -24,6 +25,7 @@ struct AllMembersView: View {
     
     @State var dragDistance: CGFloat? = nil
     @State private var buyButtonsSize: CGSize = .zero
+    @State private var relationshipChartSize: CGSize = .zero
     
     private let columns = [
         GridItem(.flexible(minimum: 10, maximum: .infinity),spacing: 0),
@@ -32,6 +34,38 @@ struct AllMembersView: View {
     var hasBuyer: Bool {
         coffeeculeManager.selectedBuyer != nil
     }
+    
+    @State private var timerCancellable: AnyCancellable?
+    @State var opacity: CGFloat = 0
+    
+    private func lowerOpacity() {
+        timerCancellable?.cancel()
+        timerCancellable = Timer.publish(every: 0.01, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                let step: Double = 0.025
+                if self.opacity > 0.0 {
+                    self.opacity -= step
+                } else {
+                    timerCancellable?.cancel()
+                }
+            }
+    }
+    
+    private func raiseOpacity() {
+        timerCancellable?.cancel()
+        timerCancellable = Timer.publish(every: 0.01, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                let step: Double = 0.025
+                if self.opacity < 1.0 {
+                    self.opacity += step
+                } else {
+                    timerCancellable?.cancel()
+                }
+            }
+    }
+    
     var body: some View {
         GeometryReader { geo in
             VStack(spacing: 0) {
@@ -94,11 +128,8 @@ struct AllMembersView: View {
                         VStack {
                             Spacer()
                             relationshipWebChart
-                                .frame(height: (dragDistance ?? 0))
-//                                .offset(y: 40)
-                            
-                            //                            .frame(height: dragDistance ?? 0 )
-                            //                            .offset(y: dragDistance ?? 0)
+                                .frame(height: max((dragDistance ?? 0), CGFloat(0)))
+                                .opacity(opacity)
                         }
                     }
                     .background(.regularMaterial)
@@ -106,14 +137,20 @@ struct AllMembersView: View {
                     .highPriorityGesture(
                         DragGesture()
                             .onChanged { newValue in
-                                dragDistance = -newValue.translation.height + (dragDistance ?? 0)
+                                let newDistance = -newValue.translation.height + (dragDistance ?? 0)
+                                if newDistance < geo.size.height * 0.8 {
+                                    self.dragDistance = newDistance
+                                    opacity = max(0, newDistance - geo.size.height / 20) / (geo.size.height * 0.8 / 4)
+                                }
                             }
                             .onEnded { newValue in
                                 withAnimation {
                                     if (dragDistance ?? 0) + -newValue.predictedEndLocation.y > geo.size.height * (1/4) {
                                         dragDistance = geo.size.height / 2
+                                        raiseOpacity()
                                     } else {
                                         dragDistance = 0
+                                        lowerOpacity()
                                     }
                                 }
                             }
@@ -122,8 +159,10 @@ struct AllMembersView: View {
                         withAnimation {
                             if dragDistance == 0 {
                                 dragDistance = geo.size.height / 2
+                                raiseOpacity()
                             } else {
                                 dragDistance = 0
+                                lowerOpacity()
                             }
                         }
                     }
@@ -135,14 +174,14 @@ struct AllMembersView: View {
                     let transition = AnyTransition.move(edge: .bottom)
                     EqualWidthVStackLayout(spacing: 10) {
                         Button {
-//                            Task {
-//                                try await vm.shareCoffeecule()
-//                                if let share = await vm.repository.rootShare {
-//                                    self.share = share
-//                                    self.container = Repository.container
-//                                    isSharing = true
-//                                }
-//                            }
+                            //                            Task {
+                            //                                try await vm.shareCoffeecule()
+                            //                                if let share = await vm.repository.rootShare {
+                            //                                    self.share = share
+                            //                                    self.container = Repository.container
+                            //                                    isSharing = true
+                            //                                }
+                            //                            }
                         } label: {
                             Label("Add New Person", systemImage: "person.crop.circle.fill.badge.plus")
                                 .font(.title2)
@@ -191,21 +230,21 @@ struct AllMembersView: View {
             }
         }
         .sheet(isPresented: $viewingHistory) {
-//            TransactionHistory(vm: vm)
+            //            TransactionHistory(vm: vm)
         }
         .sheet(isPresented: $customizingCup) {
-//            CustomizeCupView(vm: vm)
+            //            CustomizeCupView(vm: vm)
         }
         .alert("Are you sure you want to delete your Coffeecule? This action is not reversable.", isPresented: $isDeletingCoffeecule) {
             HStack {
                 Button("Yes", role: .destructive) {
-//                    Task {
-//                        do {
-//                            try await vm.deleteCoffeecule()
-//                        } catch {
-//                            print(error.localizedDescription)
-//                        }
-//                    }
+                    //                    Task {
+                    //                        do {
+                    //                            try await vm.deleteCoffeecule()
+                    //                        } catch {
+                    //                            print(error.localizedDescription)
+                    //                        }
+                    //                    }
                 }
                 Button("No", role: .cancel) {
                     isDeletingCoffeecule = false
@@ -227,13 +266,13 @@ struct AllMembersView: View {
 
 extension AllMembersView {
     var relationshipWebChart: some View {
-//        Section {
-            chart(coffeeculeManager.selectedUserDebts)
-//                .frame(height: 100)
+        //        Section {
+        chart(coffeeculeManager.selectedUserDebts)
+        //                .frame(height: 100)
             .animation(.default, value: coffeeculeManager.selectedUsers)
-//                .onAppear { withAnimation { chartScale = 1 } }
-//                .onDisappear { withAnimation { chartScale = 0 } }
-//        }
+        //                .onAppear { withAnimation { chartScale = 1 } }
+        //                .onDisappear { withAnimation { chartScale = 0 } }
+        //        }
     }
     func chart(_ debt: [User : Int])  -> some View {
         let displayedDebts = debt
@@ -242,8 +281,11 @@ extension AllMembersView {
                 BarMark(
                     x: .value("person", $0.name),
                     y: .value("cups bought", displayedDebts[$0] ?? 10)
-                ).foregroundStyle(displayedDebts[$0] ?? 0 > 0 ? .blue : .red)
+                )
+                .foregroundStyle(displayedDebts[$0] ?? 0 > 0 ? .blue : .red)
             }
+            .padding(.horizontal)
+            .padding(.vertical)
         } else {
             return EmptyView()
         }
