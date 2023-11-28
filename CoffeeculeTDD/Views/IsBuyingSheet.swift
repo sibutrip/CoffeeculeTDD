4da7d859e7f20e3bbfe4e5a847a8e82d0f618d53
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-11-28 10:47:57 -0500

    increment opacity takes in + or -

--- CoffeeculeTDD/Views/IsBuyingSheet.swift
+++ CoffeeculeTDD/Views/IsBuyingSheet.swift
@@ -16,45 +16,35 @@ struct IsBuyingSheet: View {
     @Binding var isBuying: Bool
     @Environment(\.editMode) var editMode
     @EnvironmentObject var coffeeculeManager: CoffeeculeManager<CloudKitService<CKContainer>>
-    var hasBuyer: Bool {
-        coffeeculeManager.selectedBuyer != nil
-    }
     
-    
-    @State var dragDistance: CGFloat? = nil
+    @State private var dragDistance: CGFloat? = nil
     @State private var buyButtonsSize: CGSize = .zero
     @State private var relationshipChartSize: CGSize = .zero
     
     @State private var timerCancellable: AnyCancellable?
-    @State var opacity: CGFloat = 0
+    @State private var opacity: CGFloat = 0
     
-    private func lowerOpacity() {
+    var hasBuyer: Bool {
+        coffeeculeManager.selectedBuyer != nil
+    }
+        
+    private func incrementOpacity(with action: @escaping (CGFloat, CGFloat) -> CGFloat) {
         timerCancellable?.cancel()
         timerCancellable = Timer.publish(every: 0.01, on: .main, in: .common)
             .autoconnect()
             .sink { _ in
-                let step: Double = 0.05
-                if self.opacity > 0.0 {
-                    self.opacity -= step
-                } else {
-                    timerCancellable?.cancel()
+                let step: CGFloat = 0.05
+                if self.opacity >= 0 && self.opacity <= 1 {
+                    let newValue = action(self.opacity, step)
+                    let adjustedValue = max(min(1, newValue), 0)
+                    self.opacity = adjustedValue
+                    if adjustedValue == 1 || adjustedValue == 0 {
+                        timerCancellable?.cancel()
+                    }
                 }
             }
     }
     
-    private func raiseOpacity() {
-        timerCancellable?.cancel()
-        timerCancellable = Timer.publish(every: 0.01, on: .main, in: .common)
-            .autoconnect()
-            .sink { _ in
-                let step: Double = 0.05
-                if self.opacity < 1.0 {
-                    self.opacity += step
-                } else {
-                    timerCancellable?.cancel()
-                }
-            }
-    }
     var body: some View {
         if hasBuyer && !(editMode?.wrappedValue.isEditing ?? true) {
             let transition = AnyTransition.move(edge: .bottom)
@@ -104,17 +94,17 @@ struct IsBuyingSheet: View {
                         let newDistance = -newValue.translation.height + (dragDistance ?? 0)
                         if newDistance < geo.size.height * 0.8 {
                             self.dragDistance = newDistance
-                            opacity = max(0, newDistance - geo.size.height / 20) / (geo.size.height * 0.8 / 4)
+                            opacity = min(max(0, newDistance - geo.size.height / 20) / (geo.size.height * 0.8 / 4), 1)
                         }
                     }
                     .onEnded { newValue in
                         withAnimation {
                             if (dragDistance ?? 0) + -newValue.predictedEndLocation.y > geo.size.height * (1/4) {
                                 dragDistance = geo.size.height / 2
-                                raiseOpacity()
+                                incrementOpacity(with: +)
                             } else {
                                 dragDistance = 0
-                                lowerOpacity()
+                                incrementOpacity(with: -)
                             }
                         }
                     }
@@ -123,10 +113,10 @@ struct IsBuyingSheet: View {
                 withAnimation {
                     if (dragDistance ?? 0) == 0 {
                         dragDistance = geo.size.height / 2
-                        raiseOpacity()
+                        incrementOpacity(with: +)
                     } else {
                         dragDistance = 0
-                        lowerOpacity()
+                        incrementOpacity(with: -)
                     }
                 }
             }
@@ -140,15 +130,11 @@ struct IsBuyingSheet: View {
 
 extension IsBuyingSheet {
     var relationshipWebChart: some View {
-        //        Section {
         chart(coffeeculeManager.selectedUserDebts)
-        //                .frame(height: 100)
             .animation(.default, value: coffeeculeManager.selectedUsers)
-        //                .onAppear { withAnimation { chartScale = 1 } }
-        //                .onDisappear { withAnimation { chartScale = 0 } }
-        //        }
     }
-    func chart(_ debt: [User : Int])  -> some View {
+    
+    private func chart(_ debt: [User : Int])  -> some View {
         let displayedDebts = debt
         if #available(iOS 16, *) {
             return Chart(displayedDebts.keys.sorted(by: { $0.name < $1.name }), id: \.self) {
